b01c2022bc3cc7f6b408afbe5a99612bca0b17c7
Daniel Leutwyler <daniel.leutwyler@gmx.ch>
2023-08-25 18:57:53 +0200

    products component base config

---
+++ src/app/models/Product.ts
@@ -0,0 +1,13 @@
+export class Product {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+
+  constructor() {
+    this.id = 1;
+    this.title = '';
+    this.description = '';
+    this.price = 0;
+  }
+}
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../models/Product';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  title: string = 'Products List';
+  products: Product[] = [];
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.products = [
+      {
+        id: 1,
+        title: 'product 1',
+        description: 'description of product 1',
+        price: 123,
+      },
+      {
+        id: 2,
+        title: 'product 2',
+        description: 'description of product 2',
+        price: 123,
+      },
+      {
+        id: 3,
+        title: 'product 3',
+        description: 'description of product 3',
+        price: 123,
+      },
+      {
+        id: 4,
+        title: 'product 4',
+        description: 'description of product 4',
+        price: 123,
+      },
+      {
+        id: 5,
+        title: 'product 5',
+        description: 'description of product 5',
+        price: 123,
+      },
+      {
+        id: 6,
+        title: 'product 6',
+        description: 'description of product 6',
+        price: 123,
+      },
+    ];
+  }
 }
