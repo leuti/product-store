6967de3cb329c7aca26c5549030c7ecad7684a52
Daniel Leutwyler <daniel.leutwyler@gmx.ch>
2023-08-25 18:58:52 +0200

    ProductItem created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
+import { ProductItemComponent } from './product-item/product-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductsComponent
+    ProductsComponent,
+    ProductItemComponent
   ],
   imports: [
     BrowserModule,
